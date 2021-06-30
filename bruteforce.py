329b6694d4cbbfcc047bcc590ecfd4decdd4a416
Aksel <axel.akonde@gmail.com>
2021-07-01 01:17:50 +0200

    fonction principale find_best_portfolio (paas finie), fonctions annexes de calcul: porfolio cost, filter per cost, average ROI, index ROI/Cost. Pas d'utilisation de lambda, ET que des prints pour le moment, pas de sauvegarde fichier non plus pour le moment

--- bruteforce.py
+++ bruteforce.py
@@ -23,31 +23,67 @@ recupérer la liste des actions via l'import d'un fichier csv ?
         -> on ne garde que celui qui a le meilleur score sur les deux portefeuilles
 """
 
-"""
-# juste un test à supprimer!
-def first_test(iterable, r):
-    test = combinations(iterable, r)
-    with open('tests/test.txt', 'w') as file:
-        [file.write(str(f'{t}\n')) for t in test]
-"""
 
-
-def get_average_roi(portfolio: Union[tuple, list]):  # peut etre juste tuple en fait
+def get_portfolio_cost(portfolio: tuple):
+    #  ajouter la notion de cout du portefeuille dans le calcul de l'indice ? si oui comment ?
+    #  si on garde, expliquer la formule average_roi / portfolio_cost et voir comment on selectionne le meilleur indice cost/ROI
     """
-    enables to evaluate the Return on investment of the portfolio after two years
+    calculates the total cost of a portfolio based on the shares prices
+    Helps to calculate the Return on Investment after two years
+    """
+    portfolio_cost = 0
+    for share in portfolio:
+        portfolio_cost += share['cost']
+    return portfolio_cost
+
+
+def get_portfolio_average_roi(portfolio: tuple):
+    """
+    helps calculate the ROI/Cost index of a share
+    :param portfolio:
+    :return: portfolio_average_roi
     """
     shares_roi_sum = 0
-    # portfolio_total_cost = 0   #  ajouter la notion de cout du portefeuille dans le calcul de l'indice ? si oui comment ?
     for share in portfolio:
         print(f"Share Name: {share['name']} , Cost: {share['cost']}, ROI: {share['roi']}")  # à enlever ensuite
         shares_roi_sum += share['roi']
-        # portfolio_total_cost += share['cost']
-    portfolio_average_roi = shares_roi_sum / len(portfolio)
-    print(f"Portfolio Average ROI: {portfolio_average_roi}\n")  # à enlever ensuite
+        portfolio_average_roi = round(shares_roi_sum / len(portfolio), 4)
     return portfolio_average_roi
 
 
-def get_all_possible_portfolios(shares_list: list[dict], replacement: bool = False) -> list[tuple]:  # à retravailler
+def get_portfolio_roi_cost_index(portfolio: tuple):
+    """
+    enables to evaluate the Return on investment of the portfolio after two years
+    """
+    portfolio_average_roi = get_portfolio_average_roi(portfolio)
+    portfolio_cost = get_portfolio_cost(portfolio)
+    portfolio_roi_cost_index = round(portfolio_average_roi / portfolio_cost, 6)
+    print(f'Portfolio Cost: {portfolio_cost}')
+    print(f"Portfolio Average ROI: {portfolio_average_roi}")  # à enlever ensuite
+    print(f"Portfolio ROI/Cost index : {portfolio_roi_cost_index}\n")  # à enlever ensuite
+    return portfolio_roi_cost_index
+
+
+def filter_cost_acceptable_portfolio(portfolio: tuple):
+    """
+    Checks whether a portfolio is under 500€ or not
+    :param portfolio:
+    :return:
+    """
+    portfolio_total_cost = 0
+    for share in portfolio:
+        portfolio_total_cost += share['cost']
+    if portfolio_total_cost <= 500:
+        print(f'Great Portfolio under 500€: {portfolio_total_cost}€')
+        print(portfolio)  # à enlever ensuite
+        print(len(portfolio))  # à enlever ensuite
+        return portfolio
+    else:
+        print(f'this portfolio represents more than 500€ investment: {portfolio_total_cost}€')
+        return False
+
+
+def find_best_portfolio(shares_list: list[dict], replacement: bool = False) -> list[tuple]:  # à retravailler
     """
     Returns all possible combinations of shares under the given criteria:
     - Cost of portfolio under 500€
@@ -56,27 +92,29 @@ def get_all_possible_portfolios(shares_list: list[dict], replacement: bool = Fal
     Give the possibility to choose whether an share can be
     bought several times (if needed later for evolution)
     """
-    all_possible_portfolios = []
-    for shares_amount in range(1, 3): # à changer pour (1, 21) (pour arriver à 20 shares !) (tester en changeant les ranges de 1 à 21
+    for shares_amount in range(1, 2):
+    #  à changer pour (1, 21) (pour arriver à 20 shares !) (tester en changeant les ranges de 1 à 21
         if replacement:
-            generator = combinations_with_replacement(shares_list, shares_amount)
+            generator = combinations_with_replacement(shares_list, shares_amount)  # facultatif, par défaut en False
         else:
             generator = combinations(shares_list, shares_amount)
+        all_acceptable_portfolios = []
         for portfolio in generator:
-            portfolio_total_cost = 0
-            for share in portfolio:
-                portfolio_total_cost += share['cost']
-            if portfolio_total_cost <= 500:
-                print(f'Great Portfolio under 500€: {portfolio_total_cost}€')
-                all_possible_portfolios.append(portfolio)
-                print(portfolio)  # à enlever ensuite
-                print(len(portfolio))  # à enlever ensuite
-            else:
-                print(f'this portfolio represents more than 500€ investment: {portfolio_total_cost}€')
-    print(f'Amount of Possible Portfolios: {len(all_possible_portfolios)}')  # à enlever ensuite
-    return all_possible_portfolios
+            if portfolio:
+                all_acceptable_portfolios.append(filter_cost_acceptable_portfolio(portfolio))
+        print(f'Amount of Possible Portfolios: {len(all_acceptable_portfolios)}\n')  # à enlever ensuite
+        for portfolio in all_acceptable_portfolios:
+            get_portfolio_roi_cost_index(portfolio)
+
+    return all_acceptable_portfolios
 
 
+
+
+
+
+
+"""
 def find_best_portfolio(shares_list: list[dict]):  # ajouter notions de filtre montant fortefeuille et de score via des lambdas
     for shares_amount in range(17, 21):
         generator = combinations(shares_list, shares_amount)
@@ -94,30 +132,30 @@ def find_best_portfolio(shares_list: list[dict]):  # ajouter notions de filtre m
                 print('grrr')
                 # skip automatically !
     return acceptable_portfolios_list
+"""
 
+"""
+with open('tests/test.txt', 'r+') as file:
+    # voir s'il y a deja un portefeuille dans le fichier
+    # si oui comparer les deux 
+    # si le deuxieme a un meilleur indice de rentabilité remplacer le portefeuille enregistré dans le fichier 
+    previous_portfolio = file.read()
+    print(previous_portfolio)
+    if tuple(previous_portfolio) != ():
+        if get_average_roi(tuple(previous_portfolio)) <= get_average_roi(portfolio):
+            print(portfolio)  # à enlever ensuite
+            print(len(portfolio))  # à enlever ensuite
+            print("this portfolio is better than the previous one, let's keep it !")
 
-    """
-    with open('tests/test.txt', 'r+') as file:
-        # voir s'il y a deja un portefeuille dans le fichier
-        # si oui comparer les deux 
-        # si le deuxieme a un meilleur indice de rentabilité remplacer le portefeuille enregistré dans le fichier 
-        previous_portfolio = file.read()
-        print(previous_portfolio)
-        if tuple(previous_portfolio) != ():
-            if get_average_roi(tuple(previous_portfolio)) <= get_average_roi(portfolio):
-                print(portfolio)  # à enlever ensuite
-                print(len(portfolio))  # à enlever ensuite
-                print("this portfolio is better than the previous one, let's keep it !")
-
-                file.write(str(portfolio))
-                print('portfolio saved')
-            else:
-                print('oups !')
-                continue
-        else:
             file.write(str(portfolio))
             print('portfolio saved')
-    """
+        else:
+            print('oups !')
+            continue
+    else:
+        file.write(str(portfolio))
+        print('portfolio saved')
+"""
 
 
 
@@ -134,7 +172,9 @@ test_portfolio = sample_values.test_portfolio
 
 
 # functions execution
-portfolios = get_all_possible_portfolios(shares)
+# portfolios = \
+find_best_portfolio(shares)
+
 #find_best_portfolio(shares)
-for portfolio in portfolios:
-    get_average_roi(portfolio)
+#for portfolio in portfolios:
+#    get_portfolio_roi_cost_index(portfolio)
