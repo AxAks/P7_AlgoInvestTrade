f4c4ebbc6ac6de235cc7aa16b165aeb5b8d66fac
Aksel <axel.akonde@gmail.com>
2021-07-01 09:23:56 +0200

    essai save best share dans un fichier

--- bruteforce.py
+++ bruteforce.py
@@ -16,25 +16,28 @@ recupérer la liste des actions via l'import d'un fichier csv ?
         -> Si au dessus, on vire , si en dessous on garde (+ ou - 500€)
     -> calculer le score de rentablitié (gains nets /invest "ROI")
     (quid de l'investissement de départ, depend du client/choix !!)
-    (profil prudent: investir moins, moins de rentabilité, profil risque: investir plus: gain possible plus grands, quel choix)
+    (profil prudent: investir moins, moins de rentabilité, profil risque: investir plus: gain possible plus grands,
+    quel choix)
     mettre la possibilité d'ajouter un parametre de choix de "type de profil, type d'investisseur"?
     dans un 1er temps, on reste sur le pourcentage de rentabilité pour faire simple
-        -> si premier en dessous de la limite de montant on garde(pas de comparaison possible), si suivant, on compare avec le précedent.
+        -> si premier en dessous de la limite de montant on garde(pas de comparaison possible), si suivant,
+         on compare avec le précedent.
         -> on ne garde que celui qui a le meilleur score sur les deux portefeuilles
 """
 
 
-def get_portfolio_roi_cost_index(portfolio: tuple):  # faux !? ce qui compte c'est pas le ratio, mais le montant réél initial en euros
+def get_portfolio_roi_cost_index(portfolio: tuple):
+    #  faux !? ce qui compte c'est pas le ratio, mais le montant réél initial en euros
     """
     enables to evaluate the Return on investment of the portfolio after two years
     takes into account the initial cost of the portfolio
     """
     portfolio_average_roi = get_portfolio_average_roi(portfolio)
     portfolio_cost = get_portfolio_cost(portfolio)
-    portfolio_roi_cost_index = round(portfolio_average_roi / portfolio_cost, 6)
+    portfolio_roi_cost_index = round(portfolio_average_roi - portfolio_cost, 6)
     print(f'Portfolio Cost: {portfolio_cost}')  # à enlever ensuite
     print(f"Portfolio Average ROI: {portfolio_average_roi}")  # à enlever ensuite
-    print(f"Portfolio ROI/Cost index : {portfolio_roi_cost_index}\n")  # à enlever ensuite
+    print(f"Portfolio ROI - Cost index : {portfolio_roi_cost_index}\n")  # à enlever ensuite
     return portfolio_roi_cost_index
 
 
@@ -55,7 +58,8 @@ def get_portfolio_average_roi(portfolio: tuple):
 
 def get_portfolio_cost(portfolio: tuple):
     #  ajouter la notion de cout du portefeuille dans le calcul de l'indice ? si oui comment ?
-    #  si on garde, expliquer la formule average_roi / portfolio_cost et voir comment on selectionne le meilleur indice cost/ROI
+    #  si on garde, expliquer la formule average_roi / portfolio_cost et voir comment on
+    #  selectionne le meilleur indice cost/ROI
     """
     calculates the total cost of a portfolio based on the shares prices
     Helps to calculate the Return on Investment after two years
@@ -79,8 +83,31 @@ def filter_cost_acceptable_portfolio(portfolio: tuple):
         print(len(portfolio))  # à enlever ensuite
         return portfolio
     else:
-        print(f'this portfolio represents more than 500€ investment: {portfolio_total_cost}€')
+        print(f'this portfolio represents more than 500€ investment: {portfolio_cost}€')
         return False
+
+
+def save_best_portfolio(all_acceptable_portfolios):
+    with open('tests/test.txt', 'r+') as file:
+        previous_portfolio = file.read()
+        print(previous_portfolio)
+        for portfolio in all_acceptable_portfolios:
+            if previous_portfolio is '':
+                file.write(str(portfolio))
+                print(f'there is no previous portfolio saved. We save this one: {portfolio}')
+            else:
+                if get_portfolio_average_roi(tuple(previous_portfolio)) <= get_portfolio_average_roi(portfolio):
+                    print(f'Previous Portfolio: {previous_portfolio}')
+                    print(f'New Portfolio: {portfolio}')  # à enlever ensuite
+                    print(len(portfolio))  # à enlever ensuite
+                    print("the new portfolio is better than the previous one, let's keep it !")
+                    file.write(str(portfolio))
+                    print('new portfolio saved')
+                else:
+                    print(f'The previous Portfolio is better, We keep this one: {previous_portfolio}')
+                # voir s'il y a deja un portefeuille dans le fichier
+                # si oui comparer les deux
+                # si le deuxieme a un meilleur indice de rentabilité remplacer le portefeuille enregistré dans le fichier
 
 
 def find_best_portfolio(shares_list: list[dict], replacement: bool = False) -> list[tuple]:  # à retravailler
@@ -105,61 +132,11 @@ def find_best_portfolio(shares_list: list[dict], replacement: bool = False) -> l
         print(f'Amount of Possible Portfolios: {len(all_acceptable_portfolios)}\n')  # à enlever ensuite
         for portfolio in all_acceptable_portfolios:
             get_portfolio_roi_cost_index(portfolio)
-
     return all_acceptable_portfolios
 
 
-
-
-
-
-
 """
-def find_best_portfolio(shares_list: list[dict]):  # ajouter notions de filtre montant fortefeuille et de score via des lambdas
-    for shares_amount in range(17, 21):
-        generator = combinations(shares_list, shares_amount)
-        acceptable_portfolios_list = []
-        for portfolio in generator:
-            portfolio_cost = lambda x: sum([x['cost']])
-            if portfolio_cost <= 500:
-                print(f'Great Portfolio under 500€: {portfolio_cost}€')
-                acceptable_portfolios_list.append(portfolio)
-                # keep !
-                # une fonction lambda pour voir si le portefeuille est acceptable
-                # une fonction lambda pour definir son score de rentabilité
-            else:
-                print(f'this portfolio represents more than 500€ investment: {portfolio_cost}€')
-                print('grrr')
-                # skip automatically !
-    return acceptable_portfolios_list
-"""
-
-"""
-with open('tests/test.txt', 'r+') as file:
-    # voir s'il y a deja un portefeuille dans le fichier
-    # si oui comparer les deux 
-    # si le deuxieme a un meilleur indice de rentabilité remplacer le portefeuille enregistré dans le fichier 
-    previous_portfolio = file.read()
-    print(previous_portfolio)
-    if tuple(previous_portfolio) != ():
-        if get_average_roi(tuple(previous_portfolio)) <= get_average_roi(portfolio):
-            print(portfolio)  # à enlever ensuite
-            print(len(portfolio))  # à enlever ensuite
-            print("this portfolio is better than the previous one, let's keep it !")
-
-            file.write(str(portfolio))
-            print('portfolio saved')
-        else:
-            print('oups !')
-            continue
-    else:
-        file.write(str(portfolio))
-        print('portfolio saved')
-"""
-
-
-
-"""
+# Example
 # Program to show the use of lambda functions
 
 double = lambda x: x * 2
