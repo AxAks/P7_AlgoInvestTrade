687775409ee839370bdbc7176f5ac68cb10a8346
Axel <axel.akonde@cinego.net>
2021-07-02 16:45:58 +0200

    en gros chantier! fonctions save et load a ecrire fonction principale à reprendre, ecrire et sortir les fonctions with_open read/write etc...)

--- bruteforce.py
+++ bruteforce.py
@@ -25,13 +25,16 @@ recupérer la liste des actions via l'import d'un fichier csv ?
         -> on ne garde que celui qui a le meilleur score sur les deux portefeuilles
 """
 
-
+"""
+# je voulais prendre en compte le cout d'investissement de départ (mais à revoir, pas utile pour le projet) 
+# // donner ensuite la possibilité de choix entre option : profil prudent/ profil prise de risque
+# -> favoriser un cout d'investimment moindre OU favoriser le profit maximum meme si le cout de déparrt est plus elevé 
 def get_portfolio_roi_cost_index(portfolio: tuple):
     #  faux !? ce qui compte c'est pas le ratio, mais le montant réél initial en euros
-    """
-    enables to evaluate the Return on investment of the portfolio after two years
-    takes into account the initial cost of the portfolio
-    """
+"""
+    # enables to evaluate the Return on investment of the portfolio after two years
+    # takes into account the initial cost of the portfolio
+"""
     portfolio_average_roi = get_portfolio_average_roi(portfolio)
     portfolio_cost = get_portfolio_cost(portfolio)
     portfolio_roi_cost_index = round(portfolio_average_roi - portfolio_cost, 6)
@@ -39,6 +42,7 @@ def get_portfolio_roi_cost_index(portfolio: tuple):
     print(f"Portfolio Average ROI: {portfolio_average_roi}")  # à enlever ensuite
     print(f"Portfolio ROI - Cost index : {portfolio_roi_cost_index}\n")  # à enlever ensuite
     return portfolio_roi_cost_index
+"""
 
 
 def get_portfolio_average_roi(portfolio: tuple):
@@ -70,7 +74,6 @@ def get_portfolio_cost(portfolio: tuple):
     return portfolio_cost
 
 
-
 def filter_cost_acceptable_portfolio(cost: float) -> bool: # peut etre enlevé car on utilise une lambda !
     """
     Checks whether a portfolio is under 500€ or not
@@ -78,12 +81,28 @@ def filter_cost_acceptable_portfolio(cost: float) -> bool: # peut etre enlevé 
     return cost <= 500
 
 
+def load(portfolio_str: str) -> dict:
+    """
+    gets a string with the shares names of a portfolio
+    and transforms them as a portfolio of shares object with the values : cost and roi
+    """
+    pass
+
+
+def save(portfolio: dict) -> str:
+    """
+    gets a portfolio of shares with the values : name, cost and roi
+    and transforms it into a string with the shares names of a portfolio
+    """
+    pass
+
+
 def save_best_portfolio(all_acceptable_portfolios):
     with open('tests/test.txt', 'r+') as file:
         previous_portfolio = file.read()
         print(previous_portfolio)
         for portfolio in all_acceptable_portfolios:
-            if previous_portfolio is '':
+            if previous_portfolio == '': # a remplacer par [] quand on aura le serializer/deserializer
                 file.write(str(portfolio))
                 print(f'there is no previous portfolio saved. We save this one: {portfolio}')
             else:
@@ -101,7 +120,8 @@ def save_best_portfolio(all_acceptable_portfolios):
                 # si le deuxieme a un meilleur indice de rentabilité remplacer le portefeuille enregistré dans le fichier
 
 
-def find_best_portfolio(shares_list: list[dict], strength: int, filter: Callable[[Any], bool], score: Callable[[Any], float],
+def find_best_portfolio(shares_list: list[dict],
+                        strength: int, filter: Callable[[Any], bool], score: Callable[[Any], float],
                         replacement: bool = False) -> list[tuple]:  # à retravailler
     """
     Returns all possible combinations of shares under the given criteria:
@@ -111,8 +131,7 @@ def find_best_portfolio(shares_list: list[dict], strength: int, filter: Callable
     Give the possibility to choose whether an share can be
     bought several times (if needed later for evolution)
     """
-    for shares_amount in range(1, strength):
-    #  à changer pour (1, 21) (pour arriver à 20 shares !) (tester en changeant les ranges de 1 à 21
+    for shares_amount in range(strength):  # attention strength = 21 s'il y a 20 action, c'est exclusif
         if replacement:
             generator = combinations_with_replacement(shares_list, shares_amount)  # facultatif, par défaut en False
         else:
@@ -121,17 +140,31 @@ def find_best_portfolio(shares_list: list[dict], strength: int, filter: Callable
         all_acceptable_portfolios = []
         for portfolio in generator:
             if portfolio:
+                #  pour la premiere, boucle jai un tuple mais vide
+                #  je ne veux pas le prendre en compte mais du coup ca se gere pas avec un boolean
                 cost = get_portfolio_cost(portfolio)
                 if filter(cost):
                     all_acceptable_portfolios.append(portfolio)
-        print(f'Amount of Possible Portfolios: {len(all_acceptable_portfolios)}\n')  # à enlever ensuite
 
-        with open('tests/test.txt', 'r+') as file:
-            previous_portfolio = file.read()
+        with open('tests/test.txt', 'r') as file:
+            previous_portfolio_str = file.read() # -> ce sera un string avec seulement les Nom d'actions, il faut deserialiser pour obtenir les autres valeurs
+            previous_portfolio = load(previous_portfolio_str)
+            previous_portfolio_score = score(previous_portfolio)
+            #  dans le fichier on va enregistrer une liste.
+            #  mais on veut comparer des scores de liste.
+            #  Il faut une étape intermediare : pouvoir passer d'un portefeuille à un score, et du
             print(previous_portfolio)
+            print(previous_portfolio_score)
             for portfolio in all_acceptable_portfolios:
                 portfolio_score = score(portfolio)
 
+            if portfolio_score > previous_portfolio_score or previous_portfolio_str == '':
+                with open('tests/test.txt', 'w') as file:
+                    file.write(save(portfolio))
+                    print("the new portfolio is better than the previous one, let's keep it !")
+                    pass
+
+    print(f'Amount of Possible Portfolios: {len(all_acceptable_portfolios)}\n')  # à enlever ensuite
     return all_acceptable_portfolios
 
 
@@ -141,10 +174,9 @@ test_portfolio = sample_values.test_portfolio
 
 
 # functions execution
-# portfolios = \
-find_best_portfolio(shares, 15, lambda x: x <= 500, get_portfolio_average_roi)
+# portfolios = \
+find_best_portfolio(shares, 2, lambda x: x <= 500, get_portfolio_average_roi)
 
-score: Callable[[Any], float]
 # find_best_portfolio(shares)
 # for portfolio in portfolios:
 #    get_portfolio_roi_cost_index(portfolio)
