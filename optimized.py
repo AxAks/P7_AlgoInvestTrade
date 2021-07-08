d0e6f02d10dd6ef0d79f0d262bda37bc5a433155
Aksel <axel.akonde@gmail.com>
2021-07-08 02:10:15 +0200

    ajout rapide logger basique dans optimized

--- optimized.py
+++ optimized.py
@@ -4,12 +4,9 @@
 import re
 import logging
 from datetime import datetime
-from itertools import combinations, combinations_with_replacement
-from typing import Callable, Any
 
 from tests import sample_values
 
-logging.basicConfig(filename="optimized.log", level=logging.INFO, filemode='w')
 """
 recupérer la liste des actions via l'import d'un fichier csv ? 
 """
@@ -61,18 +58,24 @@ def fill_portfolio(sorted_shares_list: list) -> tuple:
     portfolio_average_roi = 0.0
     n = 0
     while portfolio_cost <= 500.0 and n < len(sorted_shares_list):  # petit bugfix/patch rapide, à faire mieux !
-        print(n)
         if portfolio_cost + get_share_cost(sorted_shares_list[n]) <= 500.0:
             portfolio.append(sorted_shares_list[n])
         portfolio_cost = get_portfolio_cost(portfolio)
         portfolio_net_roi = round(get_portfolio_net_roi(portfolio), 2)
         portfolio_average_roi = round(get_portfolio_average_roi(portfolio), 2) * 100
         print(portfolio)
-        print(f'{portfolio_cost} € (Cost)')
-        print(f'{portfolio_net_roi} € (ROI)')
-        print(f'{portfolio_average_roi} % (ROI)')
-        n += 1
+        print(f'Cost: {portfolio_cost} €')
+        print(f'Net ROI: {portfolio_net_roi} €')
+        print(f'Relative ROI: {portfolio_average_roi} %')
         print(serialize(portfolio))
+        n += 1
+        print(n)
+        print(serialize(portfolio))
+    print(portfolio)
+    print(f'Cost: {portfolio_cost} €')
+    print(f'Net ROI: {portfolio_net_roi} €')
+    print(f'Relative ROI: {portfolio_average_roi} %')
+    print(serialize(portfolio))
     return tuple(portfolio)
 
 
@@ -127,33 +130,27 @@ def get_share_score(share: dict) -> float:
     """
     return round(get_share_cost(share) * share['roi'], 5)
 
-#  -> sélectionner les actions et ajouter au portfeuille tant que le cout maximal autorisé du portfeuille n'est pas atteint
-#  la limite est le cout du portefeuille (500€)
-
 
 def main(shares_list):
+    logging.basicConfig(filename="optimized.log", level=logging.INFO, filemode='w')
     timer_0 = datetime.now()
+    logging.info(f'Scan Start: {datetime.now()}')
     sorted_shares_list = get_sorted_shares_list(shares_list)
     final_portfolio = fill_portfolio(sorted_shares_list)
     print(final_portfolio)
+    logging.info(f'Scan End: {datetime.now()}')
+    logging.info(f'Scan Result : Best Portfolio -> {serialize(final_portfolio)}')
     execution_time = datetime.now() - timer_0
     print(f' Execution Time = {execution_time}')
     return final_portfolio
 
 
+shares = sample_values.shares_list
+
 if __name__ == "__main__":
-    main(sample_values.shares_list)
-
-
+    main(shares)
 
 """
-
-def new_high_score(new_score: float, previous_score: float) -> bool:
-    # >= on a le meme ROI mais avec plus d'actions
-    # > -> on a le meme ROI avec un nombre d'actions moindre
-    return new_score > previous_score
-
-
 def read_file() -> str:
     with open('results_save.txt', 'r') as file:
         content = file.read()
@@ -163,24 +160,4 @@ def read_file() -> str:
 def write_file(_input: str) -> None:
     with open('results_save.txt', 'w') as file:
         file.write(_input)
-
-
-
-
-
-# test samples
-shares = sample_values.shares_list
-# Ou est ce qu'on doit recupérer la lsite des actions à traiter ?
-# depuis un fichier, code à adapter
-"""
-
-"""
-# functions execution
-main(shares, 1, 21, lambda x: x <= 500, get_portfolio_net_roi, secure=False)
-#  min et max ne peuvent pas etre egaux ca incl et excl
-"""
-
-"""
-if __name__ == "__main__":
-    main(shares, 1, 21, lambda x: x <= 500, get_portfolio_net_roi, secure=True)
 """
