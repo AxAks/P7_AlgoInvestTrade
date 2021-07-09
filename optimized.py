64bd1936318eae8d36252cacc429c87eada752f2
Aksel <axel.akonde@gmail.com>
2021-07-10 01:23:05 +0200

    correction pourcentages et logs dans optimized.py

--- optimized.py
+++ optimized.py
@@ -49,12 +49,12 @@ def fill_portfolio(sorted_shares_list: list) -> tuple:
             print(f'Portfolio: {serialize(portfolio)}')
             print(f'Cost: {portfolio_cost} €')
             print(f'Relative ROI: {portfolio_average_roi} %')
-            print(f'Net ROI after two years: {round(portfolio_net_roi / 100, 2)} €')
+            print(f'Net ROI after two years: {round(portfolio_net_roi, 2)} €')
         n += 1
     print(f'\nOptimized portfolio found ({len(portfolio)} shares): {serialize(portfolio)} '
           f'for a cost of: {portfolio_cost} € investment\n'
           f'-> Relative ROI: {portfolio_average_roi} %\n'
-          f'-> Net ROI after two years: {round(portfolio_net_roi / 100, 2)} €')
+          f'-> Net ROI after two years: {round(portfolio_net_roi, 2)} €')
     return tuple(portfolio)
 
 
@@ -75,7 +75,7 @@ def get_portfolio_net_roi(portfolio: tuple):
     """
     cost = get_portfolio_cost(portfolio)
     average_roi = get_portfolio_average_roi(portfolio)
-    net_roi = cost * average_roi
+    net_roi = cost * average_roi / 100
     return net_roi
 
 
@@ -122,18 +122,20 @@ def main(shares_list):
     final_portfolio = fill_portfolio(sorted_shares_list)
     logging.info(f'Scan End: {datetime.now()}')
     final_portfolio_net_roi = get_portfolio_net_roi(final_portfolio)
-    logging.info(f'Scan Result : Best Portfolio -> {serialize(final_portfolio)} '
-                 f'(Net ROI: {final_portfolio_net_roi} €)')
+    logging.info(f'Scan Result : Best Portfolio (Net ROI: {round(final_portfolio_net_roi, 2)} €):\n'
+                 f'-> {serialize(final_portfolio)}\n'
+                 f'for a investment of {get_portfolio_cost(final_portfolio)} € in {len(final_portfolio)} shares')
     execution_time = datetime.now() - timer_0
     logging.info(f'Execution Time = {execution_time}')
     print(f'\nExecution Time = {execution_time}')
     return final_portfolio
 
+
 """
 il faudrait pouvoir choisir le fichier (le passer en arg dans le terminal)...
 """
-# shares_list = sample_values.shares_list
-# shares_list = from_csv_to_list_of_dict('tests/initial_values.csv')
+#  shares_list = sample_values.shares_list
+#  shares_list = from_csv_to_list_of_dict('tests/initial_values.csv')
 shares_list = from_csv_to_list_of_dict('tests/dataset2_Python+P7.csv')
 
 if __name__ == "__main__":
