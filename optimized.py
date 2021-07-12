353fdd93732fbabbfd442681548356114c72a340
Aksel <axel.akonde@gmail.com>
2021-07-13 01:37:20 +0200

    algo optimized, un peu optimisé mais pas encore au niveau de Sienna ... et pas sur des resultats

--- optimized.py
+++ optimized.py
@@ -12,26 +12,40 @@ def fill_portfolio(sorted_shares_list: list) -> tuple:
     portfolio = []
     portfolio_cost = 0.0
     portfolio_average_roi = 0.0
+    portfolio_net_roi = 0.0
     n = 0
     while portfolio_cost <= 500.0 and n < len(sorted_shares_list):
-        next_share_name = sorted_shares_list[n]['name']
-        if portfolio_cost + get_share_cost(sorted_shares_list[n]) <= 500.0 \
-                and get_share_score(sorted_shares_list[n]) != 0.0:
+        next_share = sorted_shares_list[n]
+        next_share_name = next_share['name']
+        portfolio_net_roi = get_portfolio_net_roi(portfolio) if portfolio else 0.0
+        if portfolio_cost + get_share_cost(next_share) <= 500.0\
+                and get_share_net_roi(next_share) != 0.0\
+                and portfolio_net_roi + get_share_net_roi(next_share) > portfolio_net_roi:
             print(f'\nPortfolio cost limit not reached, new share added: {next_share_name}:')
-            portfolio.append(sorted_shares_list[n])
+            portfolio.append(next_share)
             portfolio_cost = get_portfolio_cost(portfolio)
-            portfolio_net_roi = round(get_portfolio_net_roi(portfolio), 2)
-            portfolio_average_roi = round(get_portfolio_average_roi(portfolio), 2)
+            portfolio_net_roi = get_portfolio_net_roi(portfolio)
+            portfolio_average_roi = get_portfolio_average_roi(portfolio)
             print(f'Portfolio: {serialize(portfolio)}')
-            print(f'Cost: {portfolio_cost} €')
-            print(f'Relative ROI: {portfolio_average_roi} %')
+            print(f'Cost: {round(portfolio_cost, 2)} €')
+            print(f'Relative ROI: {round(portfolio_average_roi, 2)} %')
             print(f'Net ROI after two years: {round(portfolio_net_roi, 2)} €')
         n += 1
     print(f'\nOptimized portfolio found ({len(portfolio)} shares): {serialize(portfolio)} '
-          f'for a cost of: {portfolio_cost} € investment\n'
-          f'-> Relative ROI: {portfolio_average_roi} %\n'
+          f'for a cost of: {round(portfolio_cost, 2)} € investment\n'
+          f'-> Relative ROI: {round(portfolio_average_roi, 2)} %\n'
           f'-> Net ROI after two years: {round(portfolio_net_roi, 2)} €')
     return tuple(portfolio)
+
+
+def get_share_net_roi(share: dict) -> float:
+    """
+    Calculate the net return on investment for a specific share
+    """
+    if not share:
+        return 0.0
+    else:
+        return round(get_share_cost(share) * share['roi'], 5)
 
 
 def get_share_cost(share: dict) -> float:
@@ -44,12 +58,12 @@ def get_share_cost(share: dict) -> float:
 
 def get_share_score(share: dict) -> float:
     """
-    Calculate the net return on investment for a specific share
+    Extracts the return on investment in % for a specific share
     """
     if not share:
         return 0.0
     else:
-        return round(get_share_cost(share) * share['roi'], 5)
+        return share['roi']
 
 
 def get_sorted_shares_list(shares_list: list) -> list:
@@ -79,9 +93,9 @@ def main(shares_list):
 """
 il faudrait pouvoir choisir le fichier (le passer en arg dans le terminal)...
 """
-#  shares_list = from_csv_to_list_of_dict('data/initial_values.csv')
-#  shares_list = from_csv_to_list_of_dict('data/dataset1_Python+P7.csv')
-shares_list = from_csv_to_list_of_dict('data/dataset2_Python+P7.csv')
+# shares_list = from_csv_to_list_of_dict('data/initial_values.csv')
+shares_list = from_csv_to_list_of_dict('data/dataset1_Python+P7.csv')
+# shares_list = from_csv_to_list_of_dict('data/dataset2_Python+P7.csv')
 
 if __name__ == "__main__":
     main(shares_list)
